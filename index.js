462fb3e6cafd6a16996f7b5823bd62324c229032
YRAL375 <vadim.voytkus@solbeg.com>
2020-08-04 21:15:53 +0300

    Creating server and pages

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-const chalk = require('chalk');
-const text = require('./data');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 4000;
 
-console.log(chalk.red(text));
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+    const ext = path.extname(filePath);
+    let contentType;
 
-console.log(__dirname);
+    switch (ext) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    if (!ext) {
+        filePath += '.html';
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+                if (err) {
+                    res.writeHead(500);
+                    res.end('Error');
+                } else {
+                    res.writeHead(200, {
+                        'Content-Type': contentType
+                    });
+                    res.end(data);
+                }
+            })
+        }
+
+        res.writeHead(200, {
+            'Content-Type': contentType
+        });
+        res.end(content);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Node server is listening at http://localhost:${PORT}`);
+});
